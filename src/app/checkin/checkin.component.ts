6db13117f154c4a902b26b98b237387696c5c2ab
PrinceTG80 <tgavkhare80@gmail.com>
2022-06-16 13:52:13 +0530

    Visitor table completed

--- src/app/checkin/checkin.component.ts
+++ src/app/checkin/checkin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DatabaseService } from '../database.service';
+import { SharedServiceService } from '../shared-service.service';
+import { firstValueFrom } from 'rxjs';
 @Component({
   selector: 'app-checkin',
   templateUrl: './checkin.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckinComponent implements OnInit {
 
-  constructor() { }
+  visitorData : any;
+  constructor(public db: DatabaseService, public sharedService: SharedServiceService) { }
 
   ngOnInit(): void {
+      const userId = this.sharedService.get('userId')
+      this.getData(userId)
+  }
+
+  async getData(userId: any){
+    this.visitorData = await this.db.getvisitors(userId)
+    console.log(this.visitorData)
   }
 
 }
